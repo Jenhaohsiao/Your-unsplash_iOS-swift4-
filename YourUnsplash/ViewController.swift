ddc6e89b5009ac6798d91a71afe85ffc790dc99b
JenHao <jenhao.hsiao@gmail.com>
2018-03-20 14:29:11 -0400

    Added extension function to show image on view

--- YourUnsplash/ViewController.swift
+++ YourUnsplash/ViewController.swift
@@ -30,6 +30,10 @@ struct Course:Decodable {
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var showUIimage: UIImageView!
+    
+    var picArray = [UnsplashRoot]()
+    
     var apiRootUrl:String = "https://api.unsplash.com/photos/?client_id="
     var apiAccessKey:String = "f36a3f6ba90ed4c4d1872eb8fa50e7933ce1c6b287d44af7c0953c7780953e7c"
     
@@ -43,6 +47,10 @@ class ViewController: UIViewController {
         //         let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
         
         //        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        let strUrl = "https://images.unsplash.com/photo-1521069611610-f684174f19e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjIzMDY3fQ&s=3d5f24d9487ebb08023b1e6325be09af"
+        
+        let imgUrl = URL(string: strUrl)
+        showUIimage.downloadedFrom(url: imgUrl!)
         
         let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
         
