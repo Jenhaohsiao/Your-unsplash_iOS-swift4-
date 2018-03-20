f56b76063c4783b18a22dd208fd7d6297a52dd9e
JenHao <jenhao.hsiao@gmail.com>
2018-03-20 13:49:13 -0400

    Created struct for API

--- YourUnsplash/CallUnsplash.swift
+++ YourUnsplash/CallUnsplash.swift
@@ -8,22 +8,70 @@
 
 import Foundation
 
-struct CallUnsplash:Codable{
+struct UnsplashRoot:Codable{
     
-    var id:String
-    var created_at:String
-    var updated_at:String
-    var width:Int
-    var height: Int
+    var id:String?
+    var created_at:String?
+    var updated_at:String?
+    var width:Int?
+    var height: Int?
+    var categories :[Categories]?
+    var urls:[Urls]?
+    var links:[Links]?
+    var liked_by_user:Bool?
+    var sponsored:Bool?
+    var likes:Int?
+    var user:[User]?
+    var current_user_collections:[Current_user_collections]?
     
-    struct PictureUrls:Codable {
-        var raw:URL
-        var full:URL
-        var regular:URL
-        var small:URL
-        var thumb:URL
+    
+    struct Categories:Codable {
+        
     }
     
-    var pictureUrls:[PictureUrls]
+    struct Urls:Codable {
+        var raw:URL?
+        var full:URL?
+        var regular:URL?
+        var small:URL?
+        var thumb:URL?
+    }
+    
+  
+    
+    struct Links:Codable {
+        var self_:URL?
+        var html:URL?
+        var download:URL?
+        var download_location:URL?
+    }
+    
+    struct User:Codable {
+        var id:String?
+        var updated_at:String?
+        var username:String?
+        var name:String?
+        var first_name:String?
+        var last_name:String?
+        var twitter_username:String?
+        var portfolio_url:URL?
+        var bio:String?
+        var location:String?
+//        var links:String?
+//        var profile_image:String?
+        var total_collections:Int?
+        var instagram_username:String?
+        var total_likes:Int?
+        var total_photos:Int?
+        
+    }
+    
+    struct Current_user_collections:Codable {
+        
+    }
+    
+    
+    
+    
     
 }
