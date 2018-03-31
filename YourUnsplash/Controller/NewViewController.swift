13b8ec971181d811e861fbc5dd9ce14f070fa240
JenHao <jenhao.hsiao@gmail.com>
2018-03-31 16:19:09 -0400

    Added callback function and class method

--- YourUnsplash/Controller/NewViewController.swift
+++ YourUnsplash/Controller/NewViewController.swift
@@ -23,21 +23,13 @@ class NewViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         self.newViewTableView.delegate = self
         self.newViewTableView.dataSource = self
         self.newViewTableView.separatorStyle = .none
-        
         self.newViewTableView.reloadData()
-        
-    // This function at same file. it is working
-        getSource {
-            self.newViewTableView.reloadData()
-        }
         
 //        If I use this class function, is not working.
         
-//       self.searchResultArray = ResearchGetResults.getArrayBack()
-        
-        DispatchQueue.main.async() {
-            self.newViewTableView.reloadData()
-            print("check4")
+        ResearchSearvice().getSource(keyWordFromSerchView: "") { (result) in
+            self.searchResultArray = result
+             self.newViewTableView.reloadData()
         }
  
     }
@@ -46,46 +38,41 @@ class NewViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         
     }
     
-    func getSource(completed: @escaping() -> ()){
-
-        let apiRootUrl:String = "https://api.unsplash.com/photos/?client_id="
-        let apiAccessKey:String = "f36a3f6ba90ed4c4d1872eb8fa50e7933ce1c6b287d44af7c0953c7780953e7c"
-        //        let apiSearchKeyWord:String = "&query=" +  keyWordFromSerchView
-        let apiCount:String = "&count=30"
-        
-        //        let jsonUrlString = apiRootUrl + apiAccessKey + apiSearchKeyWord + apiCount
-        let jsonUrlString = apiRootUrl + apiAccessKey + apiCount
-        print("jsonUrlString: " + jsonUrlString)
-        
-        guard let url = URL(string: jsonUrlString) else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            //perhaps check err
-            guard let data = data else {return}
-            
-            do {
-                self.searchResultArray = try JSONDecoder().decode([UnsplashRoot].self, from: data)
-                print("Got source successful")
-                
-                //Check if the result is nil or not
-                guard self.searchResultArray.count != 0 else{
-                    print("We can't get any data from Unsplash")
-                    return
-                }
-                
-                DispatchQueue.main.async{
-                    completed()
-                }
-                
-            }catch let jsonErr{
-                print("Error serializing json")
-                print(jsonErr)
-            }
-            
-            }.resume()
-    }
+//    func getSource(completed: @escaping() -> ()){
+//
+//        let apiRootUrl:String = "https://api.unsplash.com/photos/?client_id="
+//        let apiAccessKey:String = "f36a3f6ba90ed4c4d1872eb8fa50e7933ce1c6b287d44af7c0953c7780953e7c"
+//        //        let apiSearchKeyWord:String = "&query=" +  keyWordFromSerchView
+//        let apiCount:String = "&count=30"
+//
+//        //        let jsonUrlString = apiRootUrl + apiAccessKey + apiSearchKeyWord + apiCount
+//        let jsonUrlString = apiRootUrl + apiAccessKey + apiCount
+//        print("jsonUrlString: " + jsonUrlString)
+//
+//        guard let url = URL(string: jsonUrlString) else {
+//            return
+//        }
+//
+//        URLSession.shared.dataTask(with: url) { (data, response, err) in
+//            //perhaps check err
+//            guard let data = data else {return}
+//
+//            do {
+//                self.searchResultArray = try JSONDecoder().decode([UnsplashRoot].self, from: data)
+//                print("Got source successful")
+//
+//                DispatchQueue.main.async{
+//
+//                    completed()
+//                }
+//
+//            }catch let jsonErr{
+//                print("Error serializing json")
+//                print(jsonErr)
+//            }
+//
+//            }.resume()
+//    }
     
     
     //identify the segue
---
+++ YourUnsplash/Services/ResearchService.swift
@@ -0,0 +1,44 @@
+//
+//  ResearchService.swift
+//  YourUnsplash
+//
+//  Created by JenHao on 2018-03-31.
+//  Copyright © 2018 JenHao. All rights reserved.
+//
+
+import Foundation
+public class ResearchSearvice {
+    func getSource(keyWordFromSerchView:String,completed: @escaping(_ result: [UnsplashRoot]) -> ()){
+        
+        let apiRootUrl:String = "https://api.unsplash.com/photos/?client_id="
+        let apiAccessKey:String = "f36a3f6ba90ed4c4d1872eb8fa50e7933ce1c6b287d44af7c0953c7780953e7c"
+                let apiSearchKeyWord:String = "&query=" +  keyWordFromSerchView
+        let apiCount:String = "&count=30"
+        
+        let jsonUrlString = apiRootUrl + apiAccessKey + apiSearchKeyWord + apiCount
+//        let jsonUrlString = apiRootUrl + apiAccessKey + apiCount
+        print("jsonUrlString: " + jsonUrlString)
+        
+        guard let url = URL(string: jsonUrlString) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            //perhaps check err
+            guard let data = data else {return}
+            
+            do {
+                let searchResultArray = try JSONDecoder().decode([UnsplashRoot].self, from: data)
+                print("Got source successful")
+                DispatchQueue.main.async {
+                    completed(searchResultArray)
+                }
+            }catch let jsonErr{
+                print("Error serializing json")
+                print(jsonErr)
+            }
+            
+            }.resume()
+    }
+}
+
